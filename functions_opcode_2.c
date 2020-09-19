5c0c6f1e3c1cfbaef5a0502b01190791bf22df3c
poix-code <1996@holbertonschool.com>
2020-09-19 18:18:45 +0000

    Functions modified to add the 'sub' opcode

--- functions_opcode_2.c
+++ functions_opcode_2.c
@@ -71,7 +71,7 @@ void sub(stack_t **node, unsigned int line_number)
 {
 	if (length(*node) < 2)
 	{
-		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
 		free(var.buffer);
 		free_stack(*node);
 		fclose(var.f_d);
